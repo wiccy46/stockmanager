adf391c14c38e52bce51f53b1db638a698a265ab
Jiajun <eric1986_chn@yahoo.com>
2020-05-24 00:07:49 +0200

    add pipenv files to ignore

--- src/stockmanager/Ticker.py
+++ src/stockmanager/Ticker.py
@@ -31,6 +31,12 @@ VALID_PERIOD = ['1d', '5d', '1mo', '3mo', '6mo',
 VALID_INTERVAL = ['1m', '2m', '5m', '15m', '30m', '60m',
                   '90m', '1h', '1d', '5d', '1wk', '1mo', '3mo']
 
+"""
+Rework required. 
+
+Provide suitable apis and attributes rather than process df. 
+"""
+
 
 class Ticker():
     """Base class of stockmanager, here it holds all basic infomation of a particular
